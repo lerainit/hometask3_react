cabe5b4157a6268d094b3877c40448e2f622fdbf
Valeria Vitvytska <leranmu@gmail.com>
2022-09-03 09:56:57 +0300

    feat:update AppRoures cart

--- src/AppRoutes.js
+++ src/AppRoutes.js
@@ -8,7 +8,7 @@ const AppRoutes = (props) => {
     return (
      
         <Routes>
-            <Route path='/' element={   <ProductPage  products={props.products} openModal={props.openModal} addFavoritesFunc={props.addtoFavorites}  ></ProductPage>} />
+            <Route path='/' element={   <ProductPage  products={props.products} openModal={props.openModal} addFavoritesFunc={props.addFavoritesFunc}  ></ProductPage>} />
            <Route path='/favorites' element={<FavoritesPage/>} />
          
            <Route path='/cart' element={<CartPage addCards={JSON.parse(localStorage.getItem('addCards'))} deleteCartItem ={props.deleteCartItem}/>}/>
--- src/cart.jsx
+++ src/cart.jsx
@@ -8,7 +8,7 @@ const CartPage = (props) =>{
 return(
 
 <>
-<h1>Cart</h1>
+
 {props.addCards.map(({id,name,price,art,url}) =><CartItem  id ={id}  name= {name} price ={price} art ={art} url={url} deleteCartItem ={props.deleteCartItem} ></CartItem>)}
 
 </>
